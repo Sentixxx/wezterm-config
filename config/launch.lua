c27102a80579159f2df9cbcb67602c84e9999986
sentuix <1091441107@qq.com>
2026-04-16 16:58:45 +0800

    fix: initialize starship from wezterm launch

--- config/launch.lua
+++ config/launch.lua
@@ -1,3 +1,4 @@
+local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
 local options = {
@@ -6,9 +7,15 @@ local options = {
 }
 
 if platform.is_win then
-   options.default_prog = { 'pwsh', '-NoLogo' }
+   local pwsh = 'C:/Program Files/PowerShell/7/pwsh.exe'
+   local starship_config = wezterm.home_dir .. '\\.config\\starship.toml'
+   local starship_init = "$env:STARSHIP_CONFIG = '" .. starship_config .. "'; "
+      .. "Invoke-Expression (& 'C:\\Program Files\\starship\\bin\\starship.exe' init powershell)"
+   local pwsh_with_starship = { pwsh, '-NoLogo', '-NoExit', '-Command', starship_init }
+
+   options.default_prog = pwsh_with_starship
    options.launch_menu = {
-      { label = 'PowerShell Core', args = { 'pwsh', '-NoLogo' } },
+      { label = 'PowerShell Core', args = pwsh_with_starship },
       { label = 'PowerShell Desktop', args = { 'powershell' } },
       { label = 'Command Prompt', args = { 'cmd' } },
       {
